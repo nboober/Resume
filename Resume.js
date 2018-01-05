5432e50e913b6b2a48e92e5ad1885b02ce97e834
Nick Boober <nboober@terpmail.umd.edu>
2018-01-04 20:32:32 -0500

    added a new Javascript file and updated other files

---
+++ Resume.js
@@ -0,0 +1,11 @@
+let display = true;
+        const showHide = () => {
+             
+            if(display === false){
+        document.getElementById('newExp').style.visibility="hidden";
+        display = true;
+        } else {
+            document.getElementById('newExp').style.visibility="visible";
+            display = false;
+        }
+        };
