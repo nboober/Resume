94c00f527adecef5a1d8956a21880ac3d6611c3b
Nick Boober <nboober@terpmail.umd.edu>
2018-01-06 17:12:57 -0500

    updated files with new content

--- Resume.js
+++ Resume.js
@@ -1,14 +1,27 @@
-let display = true;
-        const showHide = () => {
+let displayExp = true;
+        const showHide = () => {                                                //shows and hides the 
              
-            if(display === false){
+            if(displayExp === false){
         document.getElementById('newExp').style.visibility="hidden";
-        display = true;
+        displayExp = true;
+        console.log("Exp Entry Hidden");
         } else {
             document.getElementById('newExp').style.visibility="visible";
-            display = false;
+            displayExp = false;
+            console.log("Exp Entry Shown");
         }
         };
         
+let displayRef = true;        
         
-        
+        const showHideRef = () =>{
+            if(displayRef === true){
+                document.getElementById('newRef').style.visibility="visible";
+                displayRef = false;
+                console.log("Ref Entry Shown");
+            } else {
+                document.getElementById('newRef').style.visibility="hidden";
+                displayRef = true;
+                console.log("Ref Entry Hidden");
+            }
+        };
